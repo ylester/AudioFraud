dd42c4a456f50430319162724f2e95dbbba6b218
Yesha Lester <ylester@umich.edu>
2019-06-05 18:03:22 -0700

    Starter file

---
+++ audio_fraud_detector.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score, auc, confusion_matrix
+from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+# Getting started 
+# Currently looking for data :)
+def import_audio_data(*kwargs):
+	pass
+
+def extract_features(*kwargs):
+	pass
+
+def analyze_data(*kwargs):
+	pass
+
+def detect_fraud(*kwargs):
+	pass
+
+def send_results_to_hardware(*kwargs):
+	pass
