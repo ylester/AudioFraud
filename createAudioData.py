6d9b358e5c3cfb95e71a173e33b37c62cd25c763
Sedrick Cashaw <sedrick@uber.com>
2019-06-23 22:28:12 -0700

    Split original audio files for each person into multiple files of 3 seconds intervals

--- createAudioData.py
+++ createAudioData.py
@@ -1,31 +1,35 @@
 from pydub import AudioSegment
-import os
+import os,io
 
 
-directory_in_str = "/Users/sedrick/Documents/AudioFraud/AudioFraud/data"
-directory = os.fsencode(directory_in_str)
+exe_directory_in_str = "/Users/sedrick/Documents/AudioFraud/AudioFraud"
+data_directory_in_str = "/users/sedrick/Documents/AudioFraud/AudioFraud/data"
+directory = os.fsencode(exe_directory_in_str)
 
 def createData():
     #iterate over mp3 files
+    print(os.listdir(directory))
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         #full numbers in the 1000s means seconds
-        if filename.endswith("mp3"):
+        if filename.endswith("wav"):
             start = 0
             end = 3000
             dotindex = filename.index('.')
             dir_name = filename[:dotindex]
-            sound = AudioSegment.from_mp3(file)
+            file = open(file, "rb")
+            sound = AudioSegment.from_wav(file)
             while end <= len(sound):
                 newfile = sound[start:end]
-                newfilename = filename + (start/1000) + "-" + (end/1000) + "seconds"
-                newfile.export('/Users/sedrick/Documents/AudioFraud/data/' + dir_name + '/' + newfilename + '.mp3', format="mp3")
+                newfilename = dir_name + str(int((start/1000))) + "-" + str(int((end/1000))) + "seconds.wav"
+                newfile.export(data_directory_in_str + '/' + dir_name + '/' + newfilename, format="wav")
                 start = end
                 end += 3000
             newfile = sound[end:]
-            newfilename = filename + "remainingseconds"
-            newfile.export('/Users/sedrick/Documents/AudioFraud/data/' + dir_name + '/' + newfilename + '.mp3',
-                           format="mp3")
+            newfilename = dir_name + "remainingseconds.wav"
+            newfile.export(data_directory_in_str + '/' + dir_name + '/' + newfilename,
+                           format='wav')
+            file.close()
         else:
          continue
 
