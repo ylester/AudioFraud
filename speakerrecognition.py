008923f8a46c98de60e026a36b0a0c1ebdde3ad6
Sedrick Cashaw Jr <scashaw@umich.edu>
2019-06-25 18:21:00 -0700

    Adding SpeakerRcong file to master

--- speakerrecognition.py
+++ speakerrecognition.py
@@ -1,4 +1,4 @@
-import sklearn, numpy, pandas
+import py
 
 def extract_features():
     None
@@ -8,3 +8,12 @@ def identify_speaker():
 
 # Test File
 
+def main():
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    us_voices = filter(lambda x: x.languages == ['en_US'], voices)
+    for voice in us_voices:
+        print(voice)
+
+if __name__ == '__main__':
+    main()
